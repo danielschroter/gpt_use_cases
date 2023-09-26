ad33ee610a08e637e301d4aa6eaf3dd3881df926
Daniel Schroter <daniel@schroter.biz>
2023-09-26 17:07:40 +0200

    GPT-4 Chat

--- main.py
+++ main.py
@@ -10,8 +10,23 @@ load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
 
+CHAT_HISTORY = []
+
+# pass Message into ChatGPT API .send() the answer
+
+
 
 @cl.on_message
 async def main(message: str):
-    await cl.Message(content=message).send()
+    
+    response = openai.ChatCompletion.create(
+        model = "gpt-4",
+        messages = [{"role": "system" , "content": "You are a virtual travel planning assistant. Your name is Lucie. You help with anything travel related. Use Emojis and have a friendly and enthusiastic tone. Try to answer in a few sentences."},
+                    {"role": "user", "content": message}], 
+        temperature = 1,
+    )
+    print("message:", message)
+    print("response:", response)
+    
+    await cl.Message(content=response["choices"][0]["message"]["content"]).send()
     
